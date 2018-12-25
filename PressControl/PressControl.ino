507632c2c92e0699f88829102c92a257c6453a40
Shurigin <das-999@mail.ru>
2018-12-25 22:17:32 +0300

    1. Добавлены необходимые переменные для функцианирования регулирования уровня; 2. Реализованна функция регулировки левого переднего балона

--- PressControl/PressControl.ino
+++ PressControl/PressControl.ino
@@ -1,13 +1,16 @@
 //#define potentPin 0
+//Библиотеки
+#include <MovingAverage.h>
+#include <RBD_Timer.h>
 // Переменные
 //Переменные пинов
 
 //Аналоговые входа
-int compressorPinA = 0;                                        //Аналоговый вход датчика давления в компрессоре
-int levelSensorFL = 1;                                         //Аналоговый вход датчика уровня переднего левого колеса
-int levelSensorFR = 2;                                         //Аналоговый вход датчика уровня переднего правого колеса
-int levelSensorRL = 3;                                         //Аналоговый вход датчика уровня заднего левого колеса
-int levelSensorFR = 4;                                         //Аналоговый вход датчика уровня заднего правого колеса
+int compressorPinA = 0;                                        //Назначение номера Аналогового входа датчика давления в компрессоре
+int levelSensorFL = 1;                                         //Назначение номера Аналогового входа датчика уровня переднего левого колеса
+int levelSensorFR = 2;                                         //Назначение номера Аналогового входа датчика переднего правого колеса
+int levelSensorRL = 3;                                         //Назначение номера Аналогового входа датчика заднего левого колеса
+int levelSensorRR = 4;                                         //Назначение номера Аналогового входа датчика заднего правого колеса
 
 //Дискретные входа
 
@@ -29,17 +32,75 @@ bool compressor;
 //Уставки функции compresorControlFunc()
 int pressureHighLevel = 10;                                  //Уставка верхнего давления при котором компрессор будет отключаться                        
 int pressureLowLevel = 8;                                    //Уставка нижнего давления при котором компрессор будет включаться
+int timeSetValveOn = 1000;                                   // уставка времени работы клапана
+int timeSet = 3000;                                          //уставка времени ожидания реакция системы
+
+//Уставки функции levelControlFunc()
+int levelSet = 250;                                          //переменная уставки положения уровня
+int levelDelta = 50;                                         //Дельта при которой регулятор не реагирует
+
 
 //ExponentialFilter<long> ADCFilter(5, 0);
+RBD::Timer timerValveOn;
+RBD::Timer timer;
+
+// Create moving average with alpha = 0.1f.
+MovingAverage average(0.05f);
 void setup() {
   Serial.begin(9600);
 
 }
 
 void loop() {
+  //#7скореевсего будут условия работы подвески{
+  levelControlFuncFL(timeSet, timeSetValveOn, levelSensorFL,  levelDelta, levelSet);  //регулирования левого переднего балона
+  //#3вставить фунцкию levelControlFuncFR(); регулирования правого переднего балона
+  //#4вставить фунцкию levelControlFuncRL(); регулирования левого заднего балона
+  //#5вставить фунцкию levelControlFuncRR(); регулирования правого заднего балона
+  //}
   compresorControlFunc(compressorPinA, pinDCompressor, pressureHighLevel, pressureLowLevel);
 
 }
+//#1откатать тело функции регулятора уровня
+void levelControlFuncFL(int timeSetF, int timerValveOnF, int levelSensorFLF, int levelDeltaF, int levelSetF) {
+   timer.setTimeout(timeSetF);
+   int levelIn = average.update( analogRead(levelSensorFLF) );
+   int levelInMod = map(levelIn, 0, 1023, 0, 500);
+ if (timer.onRestart()) {
+    if ( levelSetF - levelDeltaF > levelInMod ) {     //Условие включения накачки
+      digitalWrite(pinValFill, HIGH);
+      digitalWrite(pinValFL, HIGH);
+      timerValveOn.setTimeout(timerValveOnF);
+      //test = 50;
+     }
+    
+    if ( levelSetF + levelDeltaF < levelInMod ) {
+      digitalWrite(pinValEmpt, HIGH);
+      digitalWrite(pinValFL, HIGH);
+     }
+   }
+  if(timerValveOn.onRestart()) {
+    digitalWrite(pinValFill, LOW);
+    digitalWrite(pinValEmpt, LOW);
+    digitalWrite(pinValFL, LOW);
+    }
+  }
+ /* 
+ * void levelControlFuncFR(int) {
+ * 
+ * }
+ * 
+ * void levelControlFuncRL(){
+ * 
+ * }
+ * 
+ *void levelControlFuncRR(){
+ *
+ *}
+ * 
+ * 
+ * 
+ */
 void compresorControlFunc(int compressorPinAF, int pinDCompressorF, int pressureHighLevelF, int pressureLowLevelF) {
 
   int valAnalog0;                                            //Промежуточная переменная хронящая значение давления(необработанная)
